28f85517c3b8cba42e217c4fd3dcd715e8d6e917
Akhilreddy511 <akhilreddynarayana511@gmail.com>
2021-09-06 12:21:34 +0530

    Initial commit

---
+++ app/album.model.ts
@@ -0,0 +1,19 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+export class Album {
+  userId: number;
+  id: number;
+  title: string;
+
+  static asFormGroup(album: Album): FormGroup {
+    const fg = new FormGroup({
+      userId: new FormControl(album.userId, Validators.required),
+      id: new FormControl(album.id, Validators.required),
+      title: new FormControl(album.title, [
+        Validators.required,
+        Validators.pattern(/^-?(0|[1-9]\d*)?$/)
+      ])
+    });
+    return fg;
+  }
+}
